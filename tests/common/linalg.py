2e64d65c868c3822c33855b6529f31cc12371652
vikas-udupa <vudupa@andrew.cmu.edu>
2021-03-04 11:50:33 -0500

    added linalg norm and trace tests

--- tests/common/linalg.py
+++ tests/common/linalg.py
@@ -37,3 +37,38 @@ det = ulab.linalg.det(A)
 
 # det is a float containing the determinant
 print(det)
+
+## test 2-norm of a vector
+x = ulab.array([5, 2, 3])
+norm = ulab.linalg.norm(x)
+
+# norm is a float containing the 2-norm of the vector
+print(norm)
+
+## test 2-norm of a 2*2 matrix
+A = ulab.array([[-2, 5], [8, 7]])
+norm = ulab.linalg.norm(A)
+
+# norm is a float containing the 2-norm of the matrix
+print(norm)
+
+## test 2-norm of a 3*3 matrix
+A = ulab.array([[6, 1, 1], [4, -2, 5], [2, 8, 7]])
+norm = ulab.linalg.norm(A)
+
+# norm is a float containing the 2-norm of the matrix
+print(norm)
+
+## test trace of a 3*3 square matrix
+A = ulab.array([[6, 1, 1], [4, -2, 5], [2, 8, 7]])
+trace = ulab.linalg.trace(A)
+
+# trace is a float containing the trace of the matrix
+print(trace)
+
+## test trace of a 1*1 square matrix
+A = ulab.array([[4]])
+trace = ulab.linalg.trace(A)
+
+# trace is a float containing the trace of the matrix
+print(trace)
